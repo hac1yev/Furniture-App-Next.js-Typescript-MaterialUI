ad9e9939fd7b552c978c5a2f8e82da4df8649a92
hac1yev <ilkinhaciyev955@gmail.com>
2024-05-17 21:05:43 +0400

    .env

--- src/lib/connectToDB.ts
+++ src/lib/connectToDB.ts
@@ -6,14 +6,10 @@ interface ConnectionStatus {
 
 export const connectToDB = async () => {
     const connection: ConnectionStatus = { isConnected: false };
-    const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
-      };
+
     try {
         if(connection.isConnected) return;
-        const db = await mongoose.connect("mongodb+srv://hac1yev:rUKN56aPP4z9C06i@cluster0.iniiaiz.mongodb.net/furniture?retryWrites=true&w=majority&appName=Cluster0" as string, options);
+        const db = await mongoose.connect("mongodb+srv://hac1yev:rUKN56aPP4z9C06i@cluster0.iniiaiz.mongodb.net/furniture?retryWrites=true&w=majority&appName=Cluster0" as string);
         connection.isConnected = db.connections[0].readyState;
 
     } catch (error: any) {
