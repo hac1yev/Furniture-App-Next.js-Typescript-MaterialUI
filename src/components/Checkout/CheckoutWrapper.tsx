5a27fefa29b63e9cc6524e8fb93fc59d41c0bc2e
hac1yev <ilkinhaciyev955@gmail.com>
2024-05-18 14:56:50 +0400

    all

--- src/components/Checkout/CheckoutWrapper.tsx
+++ src/components/Checkout/CheckoutWrapper.tsx
@@ -4,7 +4,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { StripeElementLocale, StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
 import PaymentForm from "./PaymentForm";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe("pk_test_51P7bNhLcyE8tQLhfW4dRsu8uvp3JK8wEduoNZ4nYsWrtwIFj59QLNlFBApsuQflCAfpUiLJ9KrIFrjRoj3iHSYOW00WFljuaW8");
 
 export default function CheckoutWrapper() {
 
