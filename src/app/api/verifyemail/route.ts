83f1326eaabdb157c69104bad0b53e89867fe5ab
hac1yev <ilkinhaciyev955@gmail.com>
2024-05-30 15:49:09 +0400

    log user

--- src/app/api/verifyemail/route.ts
+++ src/app/api/verifyemail/route.ts
@@ -2,15 +2,17 @@ import { User } from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 import { connectToDB } from "@/lib/connectToDB";
 
-connectToDB();
 
 export async function POST(request: NextRequest){
+    await connectToDB();
 
     try {
         const reqBody = await request.json()
         const {token} = reqBody;
 
         const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
+
+        console.log(user);
 
         if (!user) {
             return NextResponse.json({error: "Invalid token"}, {status: 400})
--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -2,10 +2,9 @@
 
 import axios from "axios";
 import Link from "next/link";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function VerifyEmailPage() {
-
     const [token, setToken] = useState("");
     const [verified, setVerified] = useState(false);
     const [error, setError] = useState(false);
@@ -14,7 +13,6 @@ export default function VerifyEmailPage() {
         const urlToken = window.location.search.split("=")[1];
         setToken(urlToken || "");
     }, []);
-
 
     useEffect(() => {
         if(token.length > 0) {
@@ -26,7 +24,7 @@ export default function VerifyEmailPage() {
                     setError(true);
                     console.log(error);
                 }
-            }
+            };
             verifyUserEmail();
         }
     }, [token]);
