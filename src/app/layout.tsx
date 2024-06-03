9d33036630e35fce544ed42cfc458e09ce8c5af4
hac1yev <ilkinhaciyev955@gmail.com>
2024-06-03 16:25:56 +0400

    price

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ weight: '400', subsets: ["latin"] });
 const prata = Prata({ weight: '400', subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Home Decor",
+  title: "HomeDecor",
   description: "THE FURNITURE THAT DEFINES YOU",
   icons: {
     icon: '/favicon.png'
